32a1b4c26e392abb6601eefd339a1570ad79a527
Joel Schumacher <joelschum@gmail.com>
2020-02-19 07:29:57 +0100

    Bump version to v0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="makelove",  # Replace with your own username
-    version="0.0.2",
+    version="0.0.3",
     author="Joel Schumacher",
     author_email="joelschum@gmail.com",
     description="A packaging tool for [löve](https://love2d.org) games",
